7ab89a978b3438bef46c5a5dde8dc817e3523f41
weeattt <weeat0420@naver.com>
2022-08-22 08:57:55 +0900

    ukki

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const Container = styled.SafeAreaView`
  flex: 1;
  background-color: ${({ theme}) => theme.background}
  align-items: center;
- justify-content: flex-start;
+ justify-content: flex-start;  {/* 컨텐츠를 보여주는 방식이다. flex 를 받은 요소들을 정렬 하는 방법이다. */}
  `;
 {/* Text 컴포넌트에서 글자 색이 보라색으로 스타일링된 Title 이라는 새로운 컴포넌트 탄생 ! */}
 const Title = styled.Text` 
@@ -33,12 +33,12 @@ const Title = styled.Text`
 `;
  
  export default function App() {
-    const [isReady, setIsReady] = useState(false);
-    const [newTask, setNewTask] = useState('');
+    const [isReady, setIsReady] = useState(false);    /* 앱로딩용 */
+    const [newTask, setNewTask] = useState('');     /* 사용자가 입력하는 할일 텍스트 */
     const width = Dimensions.get('window').width;
     
     const [tasks, setTasks ] = useState({
-        '1': { id: '1', text: '1일1알고리즘', completed: false },
+        '1': { id: '1', text: '1일1알고리즘', completed: false },  /* 객체를 받는 훅 */
     
     });
 
@@ -78,17 +78,17 @@ const Title = styled.Text`
     };
     const _updateTask = item => {
         const currentTasks = Object.assign({}, tasks);
-        currentTasks[item.id] = item;
-        _saveTasks(currentTasks);
+        currentTasks[item.id] = item; /* 이 코드이해가 어렵지만, 결국 이 코드를 씀으로써 할일코드 배열이 어떻게 변해야하는지 보자. 논리를 위에서 바라보는것이다 */
+        _saveTasks(currentTasks); /* 어쨋든 currentTasks[item.id] 는 렌더링하는 객체요소중 하나이다. */
     };
     
 
     const _handleTextChange = text => {
-        setNewTask(text);
+        setNewTask(text);    /* 사용자가 본인이 타자를 치고 있음을 인식하게 해주는 함수 */
     }
 
     const _onBlur = () => {
-        setNewTask('');
+        setNewTask('');   /* netwask 변수에 빈문자열을 넣는다. */
     };
     
 
@@ -115,7 +115,7 @@ const Title = styled.Text`
                         .reverse()
                         .map(item => (
                              <Task
-                               key={item.id} 
+                               key={item.id} /* 아, key 값이 item.id 구나 */
                                item={item} 
                                deleteTask={_deleteTask}
                                toggleTask={_toggleTask}
